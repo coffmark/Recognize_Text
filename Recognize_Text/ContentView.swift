b77c20db19b6966fe14e2fae7b9c2a3a4b563322
RyosukeKamimura <come1234560@gmail.com>
2020-09-01 12:48:08 +0900

    Initial Commit

---
+++ Recognize_Text/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recognize_Text
+//
+//  Created by 神村亮佑 on 2020/09/01.
+//  Copyright © 2020 神村亮佑. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
