598819161d55791e9b669e601f022a17d3accf24
RyosukeKamimura <come1234560@gmail.com>
2020-09-02 13:44:49 +0900

    Example Vision Kit 🙆‍♂️

--- Recognize_Text/ContentView.swift
+++ Recognize_Text/ContentView.swift
@@ -6,11 +6,55 @@
 //  Copyright © 2020 神村亮佑. All rights reserved.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var recognizedText = "Tap button to start scanning"
+    
+    
+    @State private var isShowingCaptureView = false
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                ScrollView {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            .fill(Color.gray.opacity(0.2))
+                        
+                        Text(recognizedText)
+                            .padding()
+                    }
+                    .padding()
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        self.isShowingCaptureView = true
+                    }) {
+                        Text("Start Scanning")
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Capsule().fill(Color.blue))
+                }
+                .padding()
+            }
+            .navigationBarTitle("Text Recognition")
+                
+                
+            .sheet(isPresented: $isShowingCaptureView) {
+                
+                
+                ScanDocumentView(recognizedText: self.$recognizedText)
+            }
+        }
     }
 }
 
---
+++ Recognize_Text/ScanDocumentView.swift
@@ -0,0 +1,100 @@
+//
+//  ScanDocumentView.swift
+//  Recognize_Text
+//
+//  Created by 神村亮佑 on 2020/09/02.
+//  Copyright © 2020 神村亮佑. All rights reserved.
+//
+
+
+import SwiftUI
+import VisionKit
+import Vision
+import Firebase
+
+
+
+struct ScanDocumentView: UIViewControllerRepresentable {
+    
+    
+    // What's this??
+    @Environment(\.presentationMode) var presentationMode
+    
+    // Binding したrecognizedText
+    @Binding var recognizedText: String
+    
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(recognizedText: $recognizedText, parent: self)
+    }
+    
+    func makeUIViewController(context: Context) -> VNDocumentCameraViewController {
+        let documentViewController = VNDocumentCameraViewController()
+        documentViewController.delegate = context.coordinator
+        return documentViewController
+    }
+    
+    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: Context) {
+        // nothing to do here
+    }
+    
+    
+    
+    class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
+        var recognizedText: Binding<String>
+        var parent: ScanDocumentView
+        
+        init(recognizedText: Binding<String>, parent: ScanDocumentView) {
+            self.recognizedText = recognizedText
+            self.parent = parent
+        }
+        
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+            let extractedImages = extractImages(from: scan)
+            let processedText = recognizeText(from: extractedImages)
+            recognizedText.wrappedValue = processedText
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        fileprivate func extractImages(from scan: VNDocumentCameraScan) -> [CGImage] {
+            var extractedImages = [CGImage]()
+            for index in 0..<scan.pageCount {
+                let extractedImage = scan.imageOfPage(at: index)
+                guard let cgImage = extractedImage.cgImage else { continue }
+                
+                extractedImages.append(cgImage)
+            }
+            return extractedImages
+        }
+        
+        fileprivate func recognizeText(from images: [CGImage]) -> String {
+            var entireRecognizedText = ""
+            
+            
+            let recognizeTextRequest = VNRecognizeTextRequest { (request, error) in
+                guard error == nil else { return }
+                
+                guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+                
+                let maximumRecognitionCandidates = 1
+                for observation in observations {
+                    guard let candidate = observation.topCandidates(maximumRecognitionCandidates).first else { continue }
+                    
+                    entireRecognizedText += "\(candidate.string)\n"
+                    
+                }
+            }
+            recognizeTextRequest.recognitionLevel = .accurate
+            
+            for image in images {
+                let requestHandler = VNImageRequestHandler(cgImage: image, options: [:])
+                
+                try? requestHandler.perform([recognizeTextRequest])
+            }
+            
+            return entireRecognizedText
+        }
+    }
+}
