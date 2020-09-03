427d7f8006e4e4315bed7384fbf4005ae5c9ec4b
RyosukeKamimura <come1234560@gmail.com>
2020-09-03 11:10:57 +0900

    途中からStoryBoardを召喚しようと思いましたが、厳しいのでもう一度最初から行いあす

--- Recognize_Text/SceneDelegate.swift
+++ Recognize_Text/SceneDelegate.swift
@@ -20,15 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = CameraView()
-
-        // Use a UIHostingController as window root view controller.
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ Recognize_Text/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Recognize_Text
+//
+//  Created by 神村亮佑 on 2020/09/03.
+//  Copyright © 2020 神村亮佑. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    
+    
+    
+}
